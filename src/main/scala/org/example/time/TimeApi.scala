eb6b56e0a24d7e1918e2f74a609ff2f7cb7f4dbc
Arnold <rnldlinus@gmail.com>
2023-07-19 16:55:33 +0300

    Updated the API routes

--- src/main/scala/org/example/time/TimeApi.scala
+++ src/main/scala/org/example/time/TimeApi.scala
@@ -7,7 +7,7 @@ import org.http4s.dsl.Http4sDsl
 
 class TimeApi extends Http4sDsl[IO] {
   private val printer = new TimePrinter(DateTimeFormatter.RFC_1123_DATE_TIME)
-  val service = Http4Routes.of[IO] { case GET -> Root / "datetime" / country =>
+  val service = HttpRoutes.of[IO] { case GET -> Root / "datetime" / country =>
     try {
       Ok(printer.now(country))
     } catch {
