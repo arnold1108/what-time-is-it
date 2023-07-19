1f25b42bb56cf877791819dcfc85ee409adcbac1
Arnold <rnldlinus@gmail.com>
2023-07-19 15:50:17 +0300

    Added a function to convert country into timezone

--- src/main/scala/org/example/time/TimePrinter.scala
+++ src/main/scala/org/example/time/TimePrinter.scala
@@ -7,6 +7,17 @@ class TimePrinter(formatter: DateTimeFormatter) {
     val zoneId = ZoneId.of(timezone)
     ZonedDateTime.now(zoneId)
   }
+  private def countryToTimezone(country: String): String =
+    country.toLowerCase match {
+      case "italy"   => "Europe/Rome"
+      case "uk"      => "Europe/London"
+      case "kenya"   => "Africa/Nairobi"
+      case "germany" => "Europe/Berlin"
+      case "japan"   => "Asia/Tokyo"
+      case _ =>
+        val msg = s"$country is yet covered in this application"
+        throw new IllegalArgumentException(msg)
+    }
 
   private def dateTimeToString(datetime: ZonedDateTime): String = {
     formatter.format(datetime)
